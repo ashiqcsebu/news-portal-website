9e776e86b130c57e13abec6261a97f1e2f73ef06
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-04-17 18:03:34 +0600

    firebase, react router installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeAkQrlreFrk8bcuy5-4kumcxWN8bgrcM",
+  authDomain: "news-portal-website-a0e57.firebaseapp.com",
+  projectId: "news-portal-website-a0e57",
+  storageBucket: "news-portal-website-a0e57.appspot.com",
+  messagingSenderId: "463542911733",
+  appId: "1:463542911733:web:c59b5e6f1767a9287ffbbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
